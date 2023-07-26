03b9521934f7d67625adebb9f20295d1b0659c4f
UGachuzD <ulisesgachuz07@gmail.com>
2023-07-25 20:10:02 -0600

    Ajustes finales

--- src/main/java/Views/ConversorMoneda.java
+++ src/main/java/Views/ConversorMoneda.java
@@ -19,7 +19,7 @@ public class ConversorMoneda extends javax.swing.JFrame {
     private static final String API_KEY = "007fdda99fe19fe1158aae34";
     public static BigDecimal resultado = BigDecimal.ZERO;
     private static String tituloTipoConversion = "";
-
+    Menu menu = new Menu();
     /**
      * Creates new form ConversorMoneda
      */
@@ -42,7 +42,6 @@ public class ConversorMoneda extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jCBxOpcionesMoneda = new javax.swing.JComboBox<>();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -61,8 +60,11 @@ public class ConversorMoneda extends javax.swing.JFrame {
         });
 
         jButton1.setText("Menú");
-
-        jButton2.setText("Regresar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -77,12 +79,10 @@ public class ConversorMoneda extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jCBxOpcionesMoneda, 0, 324, Short.MAX_VALUE)
                         .addGap(15, 15, 15))))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(74, 74, 74)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton2)
-                .addGap(81, 81, 81))
+                .addGap(31, 31, 31))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -92,9 +92,7 @@ public class ConversorMoneda extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jCBxOpcionesMoneda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                .addComponent(jButton1)
                 .addContainerGap(13, Short.MAX_VALUE))
         );
 
@@ -410,6 +408,12 @@ public class ConversorMoneda extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jCBxOpcionesMonedaActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        this.setVisible(false);
+        menu.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -447,7 +451,6 @@ public class ConversorMoneda extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jCBxOpcionesMoneda;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
--- src/main/java/Views/ConversorTemperatura.java
+++ src/main/java/Views/ConversorTemperatura.java
@@ -17,7 +17,8 @@ public class ConversorTemperatura extends javax.swing.JFrame {
      */
     private static StringBuilder resultado = new StringBuilder();
     private static String tituloTipoConversion = "";
-
+    Menu menu = new Menu();
+    
     public ConversorTemperatura() {
         initComponents();
         setLocationRelativeTo(null);
@@ -35,7 +36,6 @@ public class ConversorTemperatura extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jCBxOpcionesTemperatura = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -54,33 +54,29 @@ public class ConversorTemperatura extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        jButton1.setText("Regresar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
         jButton2.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
         jButton2.setText("Menú");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1)
-                    .addComponent(jCBxOpcionesTemperatura, 0, 332, Short.MAX_VALUE))
-                .addGap(35, 35, 35))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton2)
-                .addGap(66, 66, 66)
-                .addComponent(jButton1)
-                .addGap(103, 103, 103))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel1)
+                            .addComponent(jCBxOpcionesTemperatura, 0, 332, Short.MAX_VALUE))))
+                .addGap(35, 35, 35))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -89,11 +85,9 @@ public class ConversorTemperatura extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(28, 28, 28)
                 .addComponent(jCBxOpcionesTemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addContainerGap(9, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addContainerGap(15, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -273,6 +267,7 @@ public class ConversorTemperatura extends javax.swing.JFrame {
                 }
                 tituloTipoConversion = jCBxOpcionesTemperatura.getItemAt(opcion);
                 JOptionPane.showMessageDialog(null, cantidad + " Newton son "+ resultado + "°C", "Resultado: " + tituloTipoConversion, JOptionPane.INFORMATION_MESSAGE);
+               
             }
             
             case 7 -> {
@@ -296,14 +291,18 @@ public class ConversorTemperatura extends javax.swing.JFrame {
                 }
                 tituloTipoConversion = jCBxOpcionesTemperatura.getItemAt(opcion);
                 JOptionPane.showMessageDialog(null, cantidad + " Rankine son "+ resultado + "°C", "Resultado: " + tituloTipoConversion, JOptionPane.INFORMATION_MESSAGE);
+                
             }
+            
         }
+        resultado.setLength(0);
     }//GEN-LAST:event_jCBxOpcionesTemperaturaActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
+        this.setVisible(false);
+        menu.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -341,7 +340,6 @@ public class ConversorTemperatura extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jCBxOpcionesTemperatura;
     private javax.swing.JLabel jLabel1;
